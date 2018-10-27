2f58c5df56b6b30ee1cd7341eeaf35842f0a53d0
Alex <alexbowman66@gmail.com>
2018-10-27 01:04:40 -0400

    First Commit

---
+++ app/src/androidTest/java/dandy2018/duckalorange/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package dandy2018.duckalorange;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("dandy2018.duckalorange", appContext.getPackageName());
+    }
+}
